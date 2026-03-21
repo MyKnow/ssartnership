7408061c687513be99561eb97b2571c43ad20331
myknow <myknow000@gmail.com>
2026-03-21 17:33:25 +0900

    feat: 1.0.0으로 버전업
    - 사용하지 않는 파일 제거
    - 구 브라우저를 위한 favicon.ico 업데이트

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL ?? "https://ssartnership.vercel.app";
+  process.env.NEXT_PUBLIC_SITE_URL ?? "https://ssartnership.myknow.xyz";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
