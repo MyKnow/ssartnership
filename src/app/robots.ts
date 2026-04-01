46ef07196395cd3677c11d91acc381c4de5a5392
myknow <myknow000@gmail.com>
2026-04-01 14:14:54 +0900

    chore: robots.txt 추가

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,11 +5,23 @@ const siteUrl =
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/admin", "/admin/"],
-    },
+    rules: [
+      {
+        userAgent: "Yeti",
+        allow: "/",
+        disallow: ["/admin", "/admin/"],
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+        disallow: ["/admin", "/admin/"],
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin", "/admin/"],
+      },
+    ],
     sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
